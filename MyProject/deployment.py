8fa5b72e64f0b6c02421e15a64d75bc8ff08b251
My project <ahmedmaryamomar184@gmail.com>
2025-02-02 18:08:32 +0300

    my commit

--- MyProject/deployment.py
+++ MyProject/deployment.py
@@ -7,7 +7,7 @@ import dj_database_url
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
-CSRF_TRUSTED_ORIGINS = ['https//' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
